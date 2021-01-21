2271e09a6a7feee94bbae5c49645623ef8f00069
Jessé Lins <linspw@hotmail.com>
2021-01-20 22:33:25 -0300

    fix webpach and create global vars

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,4 +1,6 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-links']
+  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
 }
