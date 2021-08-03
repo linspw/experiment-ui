98c0a1055438feec63d67a6d0196897e0818841e
Jessé Correia <43552865+linspw@users.noreply.github.com>
2021-08-02 21:30:51 -0300

    feat: add semver and tests system (#1)
    * adjust
    
    * config jest
    
    * test: add to z-button
    
    * docs: update contributing file
    
    * test: update a-button
    
    * feat: add semantic versioning (#2)
    
    * base
    
    * feat: add release yml

---
+++ src/components/atoms/a-button/__tests__/a-button.spec.js
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/vue';
+import AButton from '../a-button.vue';
+
+describe('AButton', () => {
+  it('render correctly with default props', () => {
+    const { html } = render(AButton, {
+      slots: {
+        default: 'My first test',
+      },
+    });
+    expect(html()).toMatchSnapshot();
+  });
+
+  it('render correctly with different props', () => {
+    const { html } = render(AButton, {
+      slots: {
+        default: 'With many Itens',
+      },
+      props: {
+        behavior: 'block',
+        variant: 'secondary',
+        size: 'small',
+        icon: 'fas fa-circle',
+        iconColor: 'primary',
+        isRounded: true,
+      },
+    });
+    expect(html()).toMatchSnapshot();
+    //    expect(getByText('My first button').closest('button')).toHaveAttribute('disabled');
+  });
+});
