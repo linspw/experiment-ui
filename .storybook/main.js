af84a090bc5028aff361146dadf6e485775d8e6e
linspw <linspw@hotmail.com>
2021-10-29 16:04:52 -0300

    feat: adjust

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   "stories": [
+    "../src/docs/0.welcome-aurora-vue.stories.mdx",
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-design-token"
-  ]
+    "storybook-design-token",
+  ],
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -17,6 +17,6 @@ export const parameters = {
     },
   },
   designToken: {
-    files: tokenFiles
+    files: tokenFiles,
   },
 }
