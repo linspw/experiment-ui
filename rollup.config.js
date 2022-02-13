7602bcffb11fcdf1a110ab4aa30bbe335a57f9b8
Jessé Correia Lins <jesse.lins@buser.com.br>
2022-02-13 09:21:08 -0300

    feat: test

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
-import vue from 'rollup-plugin-vue'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import vue from "rollup-plugin-vue";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default [
   {
-    input: 'src/main.js',
+    input: "src/main.js",
     output: [
       {
-        format: 'esm',
-        file: 'dist/lib/core-ui.mjs'
+        format: "esm",
+        file: "dist/lib/core-ui.mjs",
       },
       {
-        format: 'cjs',
-        file: 'dist/lib/core-ui.js'
-      }
+        format: "cjs",
+        file: "dist/lib/core-ui.js",
+      },
     ],
-    plugins: [
-      vue(), peerDepsExternal()
-    ],
-  }
+    plugins: [vue(), peerDepsExternal(), nodeResolve()],
+  },
 ];
